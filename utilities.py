81e972bff24d293398ee91b7b67d251320ba3023
Karl Andersson <schitrus@gmail.com>
2026-03-31 16:24:43 +0200

    La till deformatters

--- utilities.py
+++ utilities.py
@@ -7,6 +7,10 @@ def decFormatter(dec, pos):
     #return f"{dec_deg}°{abs(dec_arcmin)}'{abs(dec_arcsec):.3f}''"
     return f"{dec_deg}:{abs(dec_arcmin)}:{abs(dec_arcsec):.6f}"
 
+def decDeformatter(dec):
+    angle, arcmin, arcsec = dec.split(':')
+    return float(angle) + float(arcmin)/60 + float(arcsec)/3600
+
 # Formatterar rektascension till string format
 def raFormatter(ra, pos): 
     ra /= 15
@@ -16,3 +20,7 @@ def raFormatter(ra, pos):
 
     #return f"{ra_hour}h{abs(ra_min)}m{abs(ra_sec):.3f}s"
     return f"{ra_hour:02d}:{abs(ra_min)}:{abs(ra_sec):.6f}"
+
+def raDeformatter(ra):
+    hour, arcmin, arcsec = ra.split(':')
+    return 15*(float(hour) + float(arcmin)/60 + float(arcsec)/3600)
